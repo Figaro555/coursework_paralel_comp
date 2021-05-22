802963de76434a9b824a3969a1429ddf92103c5d
Figaro555 <alekseyzinchenko2@gmail.com>
2021-05-22 18:48:09 +0300

    added client part for sending message

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
@@ -16,15 +14,21 @@ public class Client {
             return;
         }
 
+        MassageGetter massageGetter = new MassageGetter(socket);
+        massageGetter.start();
+
+        MassageSender massageSender = new MassageSender(socket);
+        massageSender.start();
+
 
     }
 
 }
 
 
-class GetMassage extends Thread{
+class MassageGetter extends Thread{
     private BufferedReader in;
-    public GetMassage(Socket socket) {
+    public MassageGetter(Socket socket) {
         try {
             this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
@@ -39,6 +43,38 @@ class GetMassage extends Thread{
                 request = in.readLine();
                 System.out.println(request);
 
+            } catch (IOException e) {
+                break;
+            }
+
+        }
+
+    }
+}
+
+
+class MassageSender extends Thread{
+    private BufferedReader inputUser;
+    private BufferedWriter out;
+
+    public MassageSender(Socket socket) {
+        this.inputUser = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+        }
+    }
+    @Override
+    public void run(){
+        while (true){
+            String clientRequest;
+            try {
+                clientRequest = inputUser.readLine();
+                //зчитуємо з консолі
+                if(clientRequest.equals("N")) break;
+                //та відправляємо
+                out.write(clientRequest+"\n");
+                out.flush();
             } catch (IOException e) {
                 break;
             }
