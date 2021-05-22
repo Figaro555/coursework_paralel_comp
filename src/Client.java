5b41498ae7107889aeea602da4ac2e715f97d859
Figaro555 <alekseyzinchenko2@gmail.com>
2021-05-22 18:35:09 +0300

    added client part for reading messages from server

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
+    private static final int  SERVER_PORT = 8000;
+    private static String ipAddress = "localhost";
+
+    public static void main(String[] args) {
+        Socket socket;
+        try {
+            socket = new Socket(ipAddress, SERVER_PORT);
+        } catch (IOException e) {
+            System.out.println("socket failed");
+            return;
+        }
 
 
+    }
 
+}
+
+
+class GetMassage extends Thread{
+    private BufferedReader in;
+    public GetMassage(Socket socket) {
+        try {
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+
+        }
+    }
+    @Override
+    public void run(){
+        while (true){
+            String request;
+            try {
+                request = in.readLine();
+                System.out.println(request);
+
+            } catch (IOException e) {
+                break;
+            }
+
+        }
+
+    }
 }
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -6,15 +6,12 @@ import java.util.Map;
 public class ServerThread extends Thread {
     private final File SOURCE_ROOT_FILE;
     private Map<String, LinkedList<Integer>> index;
-    private Socket socket;
     private BufferedWriter out;
     private BufferedReader in;
 
     public ServerThread(Map<String,LinkedList<Integer>> index, Socket socket, File SOURCE_ROOT_FILE) throws IOException {
         this.index = index;
-        this.socket = socket;
         this.SOURCE_ROOT_FILE = SOURCE_ROOT_FILE;
-
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
